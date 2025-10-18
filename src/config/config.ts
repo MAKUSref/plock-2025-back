6fd191a24ceeb5625007b327c3b1d6ab08aa7f4b
Bartosz Palewicz <bartosz.palewicz0@gmail.com>
2025-10-18 14:16:53 +0200

    configure db and adding prettier and eslint

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,22 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 export const config = {
   PORT: process.env.PORT || "3000",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/plock-2025"
 }
+
+export const connectToMongo = async (): Promise<void> => {
+  try {
+    await mongoose.connect(config.MONGODB_URI);
+    console.log("MongoDB connected");
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+    });
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/ping", (_, res) => {
+  res.send("pong");
+}); 
+
 app.use("/api/auth", authRouter)
 app.use("/api/notification", notificationRouter)
 
